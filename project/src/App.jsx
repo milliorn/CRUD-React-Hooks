29be0719bd504e53907aff49ec9802981e2f5801
milliorn <scottmilliorn@gmail.com>
2022-07-04 14:35:44 -0700

    the view

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
 
 const App = () => {
+  const userData = [
+    { id: 1, name: "Phil", username: "philly" },
+    { id: 2, name: "Scott", username: "scotty" },
+    { id: 3, name: "Nick", username: "nicki" },
+  ];
+
+  const [users, setUsers] = useState(userData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -12,7 +20,7 @@ const App = () => {
         <div className="flex-large">
           <h2>View users</h2>
         </div>
-        <UserTable />
+        <UserTable users={users} />
       </div>
     </div>
   );
